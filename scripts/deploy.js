b35ca6cb93308f9d8eacd05aad7c9b6106148625
tashun <tashun.chamara@gmail.com>
2023-05-27 03:39:24 +0530

    solutions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,22 +5,35 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require('hardhat');
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
 
 async function main() {
-    const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+    // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    //
+    // const lockedAmount = hre.ethers.utils.parseEther('1');
+    //
+    // const Lock = await hre.ethers.getContractFactory('Lock');
+    // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    //
+    // await lock.deployed();
+    //
+    // console.log(
+    //     `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    // );
 
-    const lockedAmount = hre.ethers.utils.parseEther('1');
+    const contract = await ethers.getContractFactory('ArraySum')
+    await contract.attach('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+    await instance1.setArray([2n ** 256n - 1n, 4n]);
+    await expect(instance.getArraySum()).to.be.reverted;
 
-    const Lock = await hre.ethers.getContractFactory('Lock');
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-    await lock.deployed();
+    const ContractFactory1 = await ethers.getContractFactory('OptimizedArraySum');
+    let instance1 = await ContractFactory1.deploy();
 
-    console.log(
-        `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-    );
+    await instance1.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
